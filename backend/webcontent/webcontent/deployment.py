d83bd4f1f5d746404ba8af333a9fbb206df4b2a8
Dylan <dylan.okyere@gmail.com>
2025-08-13 16:57:02 +0200

    allow cors from frontend url

--- backend/webcontent/webcontent/deployment.py
+++ backend/webcontent/webcontent/deployment.py
@@ -8,7 +8,9 @@ CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False 
 SECRET_KEY = os.environ['MY_SECRET_KEY']
 
-# CORS_ALLOW_ALL_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    'https://zealous-plant-07f08aa03.1.azurestaticapps.net'
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
